b64598b2feb5ea2a32d4503cc74edf8e0c654a23
Ali ALTUNTAŞ <alialtuntas1official@gmail.com>
2025-12-25 23:41:36 +0300

    Add files via upload

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakimesi
+{
+    public partial class Form1 : Form
+    {
+        //Değişkenleri Tanımlıyoruz
+        float sayi1, sayi2, sonuc;
+        string islem;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Sonuç Butonunun
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Alanları kontrol ediyoruz
+            if(string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun!");
+                return;
+            }
+
+            //Geçerli parametreleri kontrol ediyoruz
+            if (!float.TryParse(textBox1.Text, out sayi1) || !float.TryParse(textBox2.Text, out sayi2))
+            {
+                MessageBox.Show("Lütfen geçerli sayılar girin!");
+                return;
+            }
+
+            //Tür Dönüştürme
+            sayi1 = float.Parse(textBox1.Text);
+            sayi2 = float.Parse(textBox2.Text);
+            islem = Convert.ToString(comboBox1.Text);
+
+            //İşlem Kısmında kullanıcının seçtiği işlemi yerine getiriyoruz.
+            switch (islem)
+            {
+                //"+" sembolü seçildiyse sayıları topluyoruz
+                case "+":
+                    sonuc = sayi1 + sayi2;
+                    break;
+                //"-" sembolü seçildiyse sayıları çıkartıyoruz
+                case "-":
+                    sonuc = sayi1 - sayi2;
+                    break;
+                //"*" sembolü seçildiyse sayıları çarpıyoruz
+                case "*":
+                    sonuc =  sayi1 * sayi2;
+                    break;
+                //"/" sembolü seçildiyse sayıları bölüyoruz
+                case "/":
+                    sonuc = sayi1 / sayi2;
+                    break;
+                //"Üs alma" Seçildiyse ilk sayıyla ikinci sayıyının üssünü alıyoruz
+                case "Üs alma":
+                    sonuc = 1;
+                    for (int i = 0; i < sayi2; i++)
+                    {
+                        sonuc *= sayi1;
+                    }
+                    break;
+                //eğer bunlardan hiçbiri seçilmediyse ekrana uyarı veriyoruz
+                default:
+                    MessageBox.Show("Geçersiz işlem seçimi!");
+                    return;
+            }
+
+            //Alanları temizliyoruz.
+            MessageBox.Show("Sonuç: " + sonuc.ToString());
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+        }
+    }
+}
---
+++ HesapMakinesi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HesapMakimesi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HesapMakimesi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("064a3f86-8f43-4908-8556-6904f5fb8a66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
